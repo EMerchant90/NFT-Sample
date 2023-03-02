e0aa7a093f96bbeac0f6723e4dbce0f57bf3c4f8
Ejaz Merchant <12518835+EMerchant90@users.noreply.github.com>
2023-03-02 01:46:55 -0700

    change

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xE9Bf93Db5869836279Eec43442Ce2BE736e9c204";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
